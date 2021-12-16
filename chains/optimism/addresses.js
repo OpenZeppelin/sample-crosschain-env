77871c4773733a31db02a21b4a521c2997005dd5
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-12-16 13:18:22 +0100

    Add a readme

---
+++ chains/optimism/addresses.js
@@ -0,0 +1,16 @@
+module.exporst = {
+    "Lib_AddressManager":                '0x5FbDB2315678afecb367f032d93F642f64180aa3', /* L1 & L2 */
+    "ChainStorageContainer-CTC-batches": '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512', /* L1      */
+    "ChainStorageContainer-SCC-batches": '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0', /* L1      */
+    "CanonicalTransactionChain":         '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9', /* L1 & L2 */
+    "StateCommitmentChain":              '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9', /* L1      */
+    "BondManager":                       '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707', /* L1      */
+    "OVM_L1CrossDomainMessenger":        '0x0165878A594ca255338adfa4d48449f69242Eb8F', /* L1 & L2 */
+    "Proxy__OVM_L1CrossDomainMessenger": '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318', /* L1 & L2 */
+    "Proxy__OVM_L1StandardBridge":       '0x610178dA211FEF7D417bC0e6FeD39F05609AD788', /* L1      */
+    "AddressDictator":                   '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e', /* L1      */
+    "L2CrossDomainMessenger":            '0x4200000000000000000000000000000000000007', /*      L2 */
+    "OVM_Sequencer":                     '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', /*         */
+    "OVM_Proposer":                      '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', /*         */
+    "ChugSplashDictator":                '0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1', /* L1      */
+};
